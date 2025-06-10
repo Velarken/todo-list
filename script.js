8a8f8acce7d14c34498d865f1fb084fbb2c3223f
Dillon <181585418+Velarken@users.noreply.github.com>
2025-06-10 15:33:04 -0400

    Fixed JS functions to work with updated classes / id's

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const taskNameInput = document.getElementById('task-name');
 const taskDescInput = document.getElementById('task-desc');
 const taskTimeInput = document.getElementById('time-spent');
 const clearTaskBttn = document.getElementById('clearTaskBtn');
-const closeBttn = document.getElementById('saveButton')
+const closeBttn = document.getElementById('closeButton')
 
 let taskList = []; 
 let completedList = [];
@@ -38,9 +38,11 @@ addTask.addEventListener('click', () => {
 
 // Clear all tasks from storage
 function clearItems() {
-    list = [];
+    console.log("hello")
+    taskList = [];
     localStorage.removeItem(storageKey);
     renderList();
+    console.log(taskList)
 }
 // Clear All Tasks and Empty Storage;
 clearTaskBttn.addEventListener('click', clearItems());
@@ -57,13 +59,15 @@ taskForm.addEventListener('submit', (event) => {
 });
 
 function saveListData() {
+    console.log("data saved")
     localStorage.setItem(storageKey, JSON.stringify(taskList))
 }
 
 function loadListData() {
+    console.log("data loaded")
     const storedListData = localStorage.getItem(storageKey);
     if (storedListData) {
-        list = JSON.parse(storedListData);
+        taskList = JSON.parse(storedListData);
         renderList();
     }
 }
@@ -74,12 +78,10 @@ function renderList() {
         const newTask = document.createElement('div');
         newTask.classList.add('task');
         newTask.innerHTML = `
+            <input type="checkbox" id="toggle-visible">
+            <div class="reps">${taskTime}</div>
             <h5 class="title">${taskName}</h5>
-            <div class="task-container">
-                <button type="radio" id="toggle-visible"></button>
-                <div class="reps">${taskTime}</div>
-                <div class="description">${taskDesc}</div>
-            </div>
+            <div class="description">${taskDesc}</div>
         `;
         todoList.appendChild(newTask);
         pageContainer.appendChild(todoList);
@@ -96,10 +98,14 @@ function renderList() {
     clearTaskBtn.addEventListener('click', () => clearItems());
     addTaskBtn.addEventListener('click', () => {taskForm.classList.add('active');});
     addTaskDiv.appendChild(addTaskBtn);
-    addTaskBtn.appendChild(clearTaskBtn);
+    addTaskDiv.appendChild(clearTaskBtn);
     todoList.appendChild(addTaskDiv);
 }
 
 // Maybe the completed / uncompleted radio button becomes a counter
     // When finished with 1 rep, lower counter
     // If counter reaches 0, move item to completed tasks
+// When adding a new task, the checkboxes reset to unset.
+    // Could store them in the localStorage object
+    // Could rewrite the render function to not 
+        // repopulate each time a task is added
